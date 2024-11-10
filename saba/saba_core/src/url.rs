52645ec503b4970541c9794bf387b9a1b4993a83
ayumu.mochizuki <ayumu839.a@gmail.com>
2024-11-10 18:53:26 +0900

    add new url struct

--- saba/saba_core/src/url.rs
+++ saba/saba_core/src/url.rs
@@ -1,4 +1,5 @@
 use alloc::string::String;
+use alloc::string::ToString;
 
 #[derive(Debuf, Clone, PartialEq)]
 pub struct Url {
@@ -7,4 +8,16 @@ pub struct Url {
     port: String,
     path: String,
     searchpart: String,
+}
+
+impl Url {
+  pub fn new(url: String) -> self {
+    Self {
+      url,
+      host : "".to_string(),
+      port : "".to_string(),
+      path : "".to_string(),
+      searchpart : "".to_string(),
+    }
+  }
 }
