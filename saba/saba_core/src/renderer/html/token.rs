51faf71e61343903fdfda0aba26fff054948afae
ayumu.mochizuki <ayumu839.a@gmail.com>
2025-02-07 13:19:22 +0900

    空文字の場合のテストを追加

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -490,3 +490,17 @@ impl Iterator for HtmlTokenizer {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::alloc::string::ToString;
+
+  #[test]
+
+  fn test_empry() {
+    let html = "".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    assert!(tokenizer.next().is_none());
+  }
+}
