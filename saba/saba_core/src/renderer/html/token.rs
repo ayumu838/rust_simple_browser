5913dc175e9a778202cbcf8f04f11252dd822670
ayumu.mochizuki <ayumu839.a@gmail.com>
2025-02-07 13:04:58 +0900

    ScriptDataLessThanSignの実装実装

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -428,6 +428,18 @@ impl Iterator for HtmlTokenizer {
 
           return Some(HtmlToken::Char(c));
         }
+
+        State::ScriptDataLessThanSign => {
+          if c == '/' {
+            self.buf = String::new();
+            self.state = State::ScriptDataEndTagOpen;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::ScriptData;
+          return Some(HtmlToken::Char('<'));
+        }
       }
     }
   }
