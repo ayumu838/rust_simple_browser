6388c2e3f6d47f6dba5c7b2092f136f8432629c5
ayumu.mochizuki <ayumu839.a@gmail.com>
2025-02-07 13:00:46 +0900

    SelfClosingStartTagの実装

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -115,6 +115,21 @@ impl HtmlTokenizer {
       }
     }
   }
+
+  fn set_self_closing_flag(&mut self) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          ref mut self_closing,
+          attributes: _,
+        } => *self_closing = true,
+          => panic!("`latest_token` should be either_StartTag"),
+      }
+    }
+  }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -387,6 +402,18 @@ impl Iterator for HtmlTokenizer {
 
           self.reconsume = true;
           self.state = State::BeforeAttributeValue;
+        }
+
+        State::SelfClosingStartTag => {
+          if c == '>' {
+            self.set_self_closing_flag();
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
         }
       }
     }
